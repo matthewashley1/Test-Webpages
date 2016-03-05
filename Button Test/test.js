8522794f994180a4900a81980bdaec956301c065
Matthew Ashley <matthewashley1@gmail.com>
2016-03-05 02:30:01 -0500

    Added in a Test Webpage for learning buttons, button effects, CSS, and Javascript.

---
+++ Button Test/test.js
@@ -0,0 +1,28 @@
+
+function myFunction1() {
+  document.getElementById("demo").innerHTML = "Hello User";
+}
+
+function myFunction2() {
+  var x = document.getElementById("demo2");
+  x.style.fontSize = "10px";
+  x.style.color = "red";
+}
+
+function myFunction3() {
+  var x = document.getElementById("demo2");
+  x.style.fontSize = "24px";
+  x.style.color = "black";
+}
+
+function myFunction4() {
+  document.getElementById("demo").innerHTML = " ";
+}
+
+function myFunction5() {
+  document.getElementById("demo1").innerHTML = Date();
+}
+
+function myFunction6() {
+  document.getElementById("demo1").innerHTML = " ";
+}
